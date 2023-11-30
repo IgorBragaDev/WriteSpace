7d2d19bed7d855b3755ff66e9c4b98800cc9bcc7
igor <igorbragafarais@gmail.com>
2023-11-30 18:55:20 -0300

    Feat : DashboardCreateCardButton

--- frontend/startpn/src/pages/dashboard/index.jsx
+++ frontend/startpn/src/pages/dashboard/index.jsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../components/logo";
 import dropMenu from "../../images/png/Frame.png";
 import userIcon from "../../images/png/Bitmap.png";
-import "./dashboard.css";
 import messageIcon from "../../images/svg/message.svg";
 import settingIcon from "../../images/svg/setting.svg";
 import taskIcon from "../../images/png/task-square.png";
 import chessIcon from "../../images/svg/Vector.svg";
 import userIconMenu from "../../images/svg/user-icon.svg";
 import exitIcon from "../../images/svg/logout.svg";
-import { useState } from "react";
+import elipse from "../../images/svg/ellipse.svg";
+import plusSignal from "../../images/svg/mais.svg";
+import "./dashboard.css";
 const DashBoard = () => {
-  const [openSideMenu, setOpenSideMenu ] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
 
   const toggleMenu = () => {
     console.log("click");
-    console.log(openSideMenu)
+    console.log(openSideMenu);
     setOpenSideMenu(!openSideMenu);
   };
 
@@ -30,35 +33,45 @@ const DashBoard = () => {
             <img src={userIcon} alt="" />
           </div>
         </header>
-        <h1 className="text_circular_color_blue_bold">Playbooks</h1>
-        <div className={`sideMenuBar ${openSideMenu ? "open" : ""}`}>
-          <Logo width={"153px"} heigth={"39px"} />
-          <ul className="text_circular_side_menu_color_grey sideMenuItensContainer ">
-            <li className="sideMenuItens">
-              <img src={chessIcon} alt="" />
-              Playbooks
-            </li>
-            <li className="sideMenuItens">
-              <img src={taskIcon} alt="" />
-              Exemplo
-            </li>
-            <li className="sideMenuItens">
-              <img src={messageIcon} />
-              Exemplo
-            </li>
-            <li className="sideMenuItens">
-              <img src={settingIcon} alt="" />
-              Exemplo
-            </li>
-            <li className="sideMenuItens">
-              <img src={userIconMenu} alt="" />
-              Minha conta
-            </li>
-          </ul>
-          <h3 className="sideMenuItens text_circular_side_menu_color_grey">
-            <img src={exitIcon} alt="" />
-            Sair
-          </h3>
+        <div className="DashContainer">
+          <h1 className="text_circular_color_blue_bold">Playbooks</h1>
+          <div className={`sideMenuBar ${openSideMenu ? "open" : ""}`}>
+            <Logo width={"153px"} heigth={"39px"} />
+            <ul className="text_circular_side_menu_color_grey sideMenuItensContainer ">
+              <li className="sideMenuItens">
+                <img src={chessIcon} alt="" />
+                Playbooks
+              </li>
+              <li className="sideMenuItens">
+                <img src={taskIcon} alt="" />
+                Exemplo
+              </li>
+              <li className="sideMenuItens">
+                <img src={messageIcon} />
+                Exemplo
+              </li>
+              <li className="sideMenuItens">
+                <img src={settingIcon} alt="" />
+                Exemplo
+              </li>
+              <li className="sideMenuItens">
+                <img src={userIconMenu} alt="" />
+                Minha conta
+              </li>
+            </ul>
+            <h3 className="sideMenuItens text_circular_side_menu_color_grey">
+              <img src={exitIcon} alt="" />
+              Sair
+            </h3>
+          </div>
+          <div>
+            <Link>
+              <div className="ellipseButton">
+                <img  src={elipse} />
+                <img className="ellipseCircle" src={plusSignal}></img>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </>
