a5ea426c39e3ddad47c9e166a1f11953d98282e2
igor <igorbragafarais@gmail.com>
2023-11-30 16:34:44 -0300

    Feat : Side menu without animation

--- frontend/startpn/src/pages/dashboard/index.jsx
+++ frontend/startpn/src/pages/dashboard/index.jsx
@@ -2,21 +2,58 @@ import Logo from "../../components/logo";
 import dropMenu from "../../images/png/Frame.png";
 import userIcon from "../../images/png/Bitmap.png";
 import "./dashboard.css";
+import messageIcon from "../../images/svg/message.svg";
+import settingIcon from "../../images/svg/setting.svg";
+import taskIcon from "../../images/png/task-square.png";
+import chessIcon from "../../images/svg/Vector.svg";
+import userIconMenu from "../../images/svg/user-icon.svg";
+import exitIcon from "../../images/svg/logout.svg";
 const DashBoard = () => {
   return (
     <>
-      <header className="containerDashboard">
-        <div className="headerDivDashBoard">
-          <button className="dropdownButton">
-            <img src={dropMenu} alt="" />
-          </button>
-          <Logo />
-          <img src={userIcon} alt="" />
+      <div className="dashboardPageContainer">
+        <header className="containerDashboard">
+          <div className="headerDivDashBoard">
+            <button className="dropdownButton">
+              <img src={dropMenu} alt="" />
+            </button>
+            <Logo />
+            <img src={userIcon} alt="" />
+          </div>
+        </header>
+        <h1 className="text_circular_color_blue_bold">Playbooks</h1>
+        <div className="sideMenuBar">
+          <Logo  width={"153px"} heigth={"39px"}/>
+          <ul className="text_circular_side_menu_color_grey sideMenuItensContainer ">
+            <li className="sideMenuItens">
+              <img src={chessIcon} alt="" />
+              Playbooks
+            </li>
+            <li className="sideMenuItens">
+              <img src={taskIcon} alt="" />
+              Exemplo
+            </li>
+            <li className="sideMenuItens">
+              <img src={messageIcon} />
+              Exemplo
+            </li>
+            <li className="sideMenuItens">
+              <img src={settingIcon} alt="" />
+              Exemplo
+            </li>
+            <li className="sideMenuItens">
+              <img src={userIconMenu} alt="" />
+              Minha conta
+            </li>
+          </ul>
+          <h3 className="sideMenuItens text_circular_side_menu_color_grey">
+            <img src={exitIcon} alt="" />
+            Sair
+          </h3>
         </div>
-      </header>
-      <h1 className="text_circular_color_blue_bold">Playbooks</h1>
+      </div>
     </>
   );
 };
 
-export default DashBoard; 
+export default DashBoard;
